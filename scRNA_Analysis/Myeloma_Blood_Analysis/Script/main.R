5615d7dad4afebe12ac0aeb6f3a6676cbc45faeb
txx99 <tazmeen@ualberta.ca>
2025-11-24 01:20:23 -0700

    manual cluster annotation, featureplots, named cluster UMAP

--- scRNA_Analysis/Myeloma_Blood_Analysis/Script/main.R
+++ scRNA_Analysis/Myeloma_Blood_Analysis/Script/main.R
@@ -1,16 +1,22 @@
 # data was"normalized to an equal sequencing depth (approximately 32k read pairs per cell) using the cellranger aggr pipeline"
+# integrated transcriptome count data post depth equalization (depth equalization != gene count normalisation)
+
 setwd("C:\\Users\\liv_u\\Desktop\\GitHub\\Omics_Projects\\scRNA_Analysis\\Myeloma_Blood_Analysis")
 
 library(Seurat)
 library(tidyverse)
+# for faster Wilcox Rank Sum Test @FindAllMarkers():
+# install.packages('devtools')
+devtools::install_github('immunogenomics/presto')
 
-# integrated transcriptome count data post depth equalization (depth equalization != gene count normalisation)
+# load data
 df = Read10X_h5("./Data/20k_WholeBlood_Fixation_Leukocyte_Isolation_2donor_aggr_count_filtered_feature_bc_matrix.h5")
 
 # CreateSeuratObject
-# (counts = count_df, min.cells= min_cell_count_for_feature_inclusion, min.features= min features count for cell inclusion, project)
+  # (counts = count_df, min.cells= min_cell_count_for_feature_inclusion, min.features= min features count for cell inclusion, project)
 sdf = CreateSeuratObject(counts = df, min.cells = 3, min.features = 200, project="Aggr Leukocytes")
 rm(df)
+
 # Preprocessing --------------
 View(sdf@meta.data)
 range(sdf$nCount_RNA)
@@ -42,15 +48,16 @@ allgenes <- rownames(sdf)
 head(allgenes)
 sdf <- ScaleData(sdf, features=allgenes)
 str(sdf)
-# Formal class Assay5 layer
-# NormalizeData layer
-# FindVariableFeatures layer
-# ScaleData layer
+  # Formal class Assay5 layer
+  # NormalizeData layer
+  # FindVariableFeatures layer
+  # ScaleData layer
 
 # linear dim redux - PCA
 sdf <- RunPCA(sdf, features = VariableFeatures(sdf))
 # dimensionality of data
 ElbowPlot(sdf)
+
 
 # Clustering -----------------
 # 15 - 25 clusters for leukocytes
@@ -65,68 +72,98 @@ DimPlot(sdf, reduction = "umap", label = TRUE)
 sdf <- FindClusters(sdf, resolution = c(0.5, 0.7, 0.9, 1.1, 1.2, 1.3))
 
 # DimPlot(sdf, group.by = "RNA_snn_res.0.5", label=TRUE)
-# DimPlot(sdf, group.by = "RNA_snn_res.0.7", label=TRUE)
-# DimPlot(sdf, group.by = "RNA_snn_res.0.9", label=TRUE)
-DimPlot(sdf, group.by = "RNA_snn_res.1.1", label=TRUE)
-# selecting .1.1 as the most reasonable clustering resolution
-ggsave("./Data/Plots/UMAP_clustered.png")
-# DimPlot(sdf, group.by = "RNA_snn_res.1.2", label=TRUE)
-# DimPlot(sdf, group.by = "RNA_snn_res.1.3", label=TRUE)
+# # DimPlot(sdf, group.by = "RNA_snn_res.0.7", label=TRUE)
+# # DimPlot(sdf, group.by = "RNA_snn_res.0.9", label=TRUE)
+DimPlot(sdf, group.by = "RNA_snn_res.0.7", label=TRUE)
+# selecting .0.7 as the most reasonable clustering resolution
+ggsave("./Data/Plots/UMAP_clustered_07.png")
+# # DimPlot(sdf, group.by = "RNA_snn_res.1.2", label=TRUE)
+# # DimPlot(sdf, group.by = "RNA_snn_res.1.3", label=TRUE)
 
 # ID cells to their cluster
-Idents(sdf) <- "RNA_snn_res.1.1" 
+Idents(sdf) <- "RNA_snn_res.0.7" 
 head(Idents(sdf))
 
-saveRDS(sdf, file= "./Data/sdf_clustered.rds")
+saveRDS(sdf, file= "./Data/seuratObject_clustered.rds")
 
 
 
 
-# # Cell type identification/cluster (FindAllMarkers()) -----------------
-# # for faster Wil Rank Sum Test @FindAllMarkers()
-# install.packages('devtools')
-# devtools::install_github('immunogenomics/presto')
-# # find the markers that are highly expressed in each cluster -> compare each cluster against all other clusters
-#   # FindMarkers() -> compare one cluster against another cluster
-#   # FindAllMarkers() -> compare each cluster against all other clusters
-#   # FindConservedMarkers() -> find markers conserved across a condition even in different clusters  
-#     # looking at clusters of cell type and w/i cluster variations of condition type; 
-# 
-# markers<-FindAllMarkers(sdf) #cluster-based
-# 
-# FeaturePlot()
-# DotPlot()
+# Manual cluster annotation -----------------
 
-# Manual Annotation: markers vs cell signatures ----
+# find the markers that are highly expressed in each cluster -> compare each cluster against all other clusters
+  # FindMarkers() -> compare one cluster against another cluster
+  # FindAllMarkers() -> compare each cluster against all other clusters
+  # FindConservedMarkers() -> find markers conserved across a condition even in different clusters
+    # looking at clusters of cell type and w/i cluster variations of condition type;
 
-# T Cells
-# CD4 T cells: CCR7, IL7R, LST1 low
-# CD8 T cells: CD8A, CD8B
-# Naive T: CCR7, LTB, IL7R
-# Cytotoxic T: GZMB, NKG7, PRF1
-# Regulatory T: FOXP3, IL2RA, CTLA4
-# γδ T: TRDC, TRGC1/2
-# 
-# NK Cells: NKG7, GZMB, GNLY, KLRD1 (CD94), KLRF1, CD56, NCAM1
-# 
-# Monocytes
-# Classical: LYZ, S100A8, S100A9, LST1, CTSS
-# Non-classical: FCGR3A (CD16), MS4A7
-# Inflammatory: IL1B, CCL3, CCL4
-# 
-# Dendritic Cells
-# cDC1: CLEC9A, CADM1, XCR1
-# cDC2: FCER1A, CD1C
-# pDC: GZMB, IRF7, LILRA4, TCF4
-# 
-# B Cells
-# Naive B: MS4A1, CD79A/B
-# Memory B: CD27, TNFRSF13B
-# Plasmablasts: MZB1, XBP1, PRDM1
-# 
-# MAIT cells: KLRB1, SLC4A10, IL7R
-# 
-# Neutrophils (if captured): S100A8, S100A9, CSF3R, LTF
+
+# FindAllMarkers(data, # ensure default assay is RNA w DefaultAssay(data)
+#                logfc.threshold = 0.25, # 0.25 standard; min log2fc avg expression for considering a gene differentially expressed in one cluster over all the other clusters 
+#                min.pct = 0.1, # 0.1 standard; min % of cells in the cluster that must present the gene 
+#                only.pos= FALSE, # only want markers that are upregulated (not downregulated)
+#                test.use = 'DESeq2', # if you want to specify package/test to use
+#                slot = 'counts' # auto adjusts if not specified; appropriate input layer for the test being used (DESeq2 uses raw counts)
+#                )
+
+# Output Interpretation:
+# avg_log2FC = log2FC seen in current cluster
+# pct.1 = in current cluster, % of cells presenting target gene 
+# pct.2 = in all other clusters, avg % of cells presenting target gene
+
+# ideally would be using FindConservedMarkers, but both patients' data is aggregated without condition labels
+# when annotating, may need to adjust resolution or FindMarkers thresholds
+markers<-FindAllMarkers(sdf,
+                        logfc.threshold = 0.25, # min log2fc avg expression for considering a gene differentially expressed in one cluster over all the other clusters 
+                        min.pct = 0.1,
+                        only.pos = TRUE) # min % of cells in cluster that must present the gene
+
+for (i in 0:18){
+  genes <- head(markers[markers$cluster==i & markers$avg_log2FC>0, "gene"], 4)
+  assign(paste0("group_", i), genes)
+}
+# FeaturePlots of top genes of each cluster  
+print(FeaturePlot(sdf, features = group_18, min.cutoff = "q10"))
+
+
+
+# Manual Annotation: markers vs cell signatures --------
+
+# FeatuerPlots of specific cell type's markers
+geneset<- c('GZMB', 'TCF4', 'IRF7', 'CLEC4C', 'IL3RA', 'LILRA4')
+print(FeaturePlot(sdf, features = geneset, min.cutoff = "q10"))
+ggsave('./Data/Plots/Marker_FeaturePlots/pDC_markers.png')
+
+sdf<- RenameIdents(sdf,)
+
+sdf<- RenameIdents(sdf, `0`= 'Neutrophils',
+                   `1`='Interferon Neutrophils', 
+                   `2`='Inflammatory Neutrophils',
+                   `4`= 'High-stress Myeloids',
+  
+                   `13`= 'Megakaryocyte',
+                   `17`= 'Immature neutrophils',
+                   
+                   `11`= 'Naive B cell',
+                   `16` = 'Basophil',
+                   `9`= 'Tumor-activated classical monocyte',
+                   `8`= 'DC-like classical monocyte',
+                   `15`= 'TAM-like non-classical monocytes',
+                   `18`= 'Plasmacytoid DC',
+                   `3`= 'CD8+ T (naive)',
+                   `7`= 'CD4+ T (naive)',
+                   `5`= 'Regulatory T cells',
+                   `14` = 'MAIT',
+                   `12`= 'γδ T cells',
+                   `6`= "Cytotoxic T",
+                   `10`= 'NK')
+
+sdf@active.ident
+DimPlot(sdf, reduction = "umap", label = TRUE, label.size = 3, cols = 'alphabet')
+ggsave('./Data/Plots/UMAP_namedClusters_color2.png', height=8, width=10)
+
+saveRDS(sdf, file= "./Data/seuratObject_namedClusters.rds")
+
 
 
 # Subcluster Major Lineages ----
